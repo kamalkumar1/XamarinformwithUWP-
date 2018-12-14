c22f949835c475c4e6ba9318badd31c7da673bde
kamal <kamalelango15@gmail.com>
2018-12-14 15:30:25 +0530

    uwp

---
+++ GridSample/SampleLogin.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace GridSample
+{
+    public partial class SampleLogin : ContentPage
+    {
+        public SampleLogin()
+        {
+            InitializeComponent();
+        }
+    }
+}
